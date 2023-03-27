bd16323642729ae61d167a9bc40d080972981e0f
AlexisDupont2 <816522@mystma.org>
2023-03-27 14:39:20 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
 #WRITE YOUR CODE IN THIS FILE
+
+def greaterThan(x,y):
+    if (x>y):
+        return "true"
+    else:
+        return"false"
+def lessThan(x,y):
+    if (x<y):
+        return "true"
+    else:
+        return "fasle"
+def equalTo(x,y):
+    if (x==y):
+        return "true"
+    else:
+        return "fasle"
+def greaterOrEqual(x,y):
+    if (x>=y):
+        return "true"
+    else:
+        return "false"
+def lessOrEqual(x,y):
+    if (x<=y):
+        return "true"
+    else:
+        return "false"
